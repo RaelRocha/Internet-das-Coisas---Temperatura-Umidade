399409fb819268d950ebb30e6cc3068b86adb5d8
RaelRocha <114624491+RaelRocha@users.noreply.github.com>
2022-10-11 20:03:31 -0300

    Update Leitura_Temperatura_umidade_DHT22.c

--- Leitura_Temperatura_umidade_DHT22.c
+++ Leitura_Temperatura_umidade_DHT22.c
@@ -1,29 +1,55 @@
 #include <DHTesp.h>
 #include "WiFi.h"
 #include "HTTPClient.h"
+#include "PubSubClient.h"
 
+// Conexão URL
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 const String url = "https://api.thingspeak.com/update?api_key=F902XZ6K92U6WS51&";
 
+// Configuração MQTT Broker
+int port = 1883;
+const char* server = "broker.hivemq.com";
+const char* topico = "Senai_Internet_das_coisas_IDR";
+
+// HTTP
 DHTesp dhtSensor;
 HTTPClient http;
 
+//MQTT
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
 
 void setup() {
- 
+ // Inicio do setup
   Serial.begin(115200);
   Serial.println("Iniciando Setup");
   WiFi.begin(ssid, password);
-
+ // Conexão Wi-Fi
   while(WiFi.status() != WL_CONNECTED){
     delay(500);
-    Serial.println("Conecting to WiFi");
+    Serial.println("Conectando ao WiFi");
   }
   dhtSensor.setup(13,  DHTesp::DHT22);
   pinMode(14, OUTPUT);
   pinMode(23, OUTPUT);
   Serial.println("Finalizando Setup");
+
+ //Conexão Broker
+ Serial.println("Conectando ao Broker");
+ mqttClient.setServer(server, port);
+ char* clientId = "esp343#edssfsd_pok";
+
+ mqttClient.connect(clientId);
+ mqttClient.subscribe(topico);
+
+ while(!mqttClient.connected()){
+   delay(500);
+   Serial.println("*");
+ }
+ Serial.println("Broker conectado");
 }
 
 void loop() {
@@ -37,13 +63,23 @@ void loop() {
   int httpCode = http.GET();
   String payload = http.getString();
 
-    //imprimindo dados capturados
+    //imprimindo e enviando dados capturados
   Serial.println("Temperatura:" +String(temperatura) + "C");
   Serial.println("Umidade:" +String(umidade) + "%");
   Serial.println("***************************************************");
   Serial.println("HttpCode: " + String(httpCode));
   Serial.println("Payload: " + payload);
   Serial.println("***************************************************");
+
+  // Msg para MQTT
+  int msg = mqttClient.publish(topico, "{\"temperatura\": 25, \"umidade\": 70}");
+  if(!msg){
+    Serial.println("Problemas ao enviar a mensagem.");
+    mqttClient.connect("esp343#edssfsd_pok");
+    mqttClient.subscribe(topico);
+  } else{
+    Serial.println("Mensagem enviada.");
+  }
 
   delay(3000);
 
